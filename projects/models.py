0156cf833175c90a51e038fc860bb4c34f5fa858
vlearner <barotvishal12@outlook.com>
2019-08-28 19:36:20 -0500

    heroku added

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    #Images
+    image = models.ImageField(upload_to='images/')
+    #Project Summary
+    projectInfo = models.CharField(max_length=250)
+
+
